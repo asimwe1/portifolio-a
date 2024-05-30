0c45229a45ea1d74c99b180940e30e3676bb6c09
asimwe1 <landryasimwe@gmail.com>
2024-05-30 05:22:43 +0200

    adssense regulated

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="h-full w-full">
-      <div className="flex flex-col gap-20">
-        <Hero />
-        <Skills />
-        <Encryption />
-        <Projects />
-      </div>
-      <Analytics />
-    </main>
+    <html>
+      <head>
+      <script async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6172208143098274"
+      crossOrigin="anonymous"></script>
+      </head>
+      <body>
+        <main className="h-full w-full">
+          <div className="flex flex-col gap-20">
+            <Hero />
+            <Skills />
+            <Encryption />
+            <Projects />
+          </div>
+          <Analytics />
+        </main>
+
+      </body>
+    </html>
   );
 }
